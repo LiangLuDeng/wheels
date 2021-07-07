5e8b7fc707d0c0b7bf11fb5ad006cadfae3cec74
dengll <277422063@qq.com>
2021-07-07 16:01:02 +0800

    添加row col

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 
 // console.log(Button)
@@ -11,6 +13,9 @@ Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
+
 
 new Vue({
   el: '#app',
